0aa8e612fe11bcfd7501bcbb03650a3cf8b4ff88
jungsugi <sooyoung199@naver.com>
2015-11-16 23:16:11 +0900

    ㄱㄱ

--- Javepaint/src/SubFrame/BottomPanel.java
+++ Javepaint/src/SubFrame/BottomPanel.java
@@ -1,18 +1,21 @@
 package SubFrame;
-
+import SubFrame.DrawPanel;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;	//flowLayout배치(한줄로 쭉배치하기위함)
 import java.awt.BorderLayout;
-import java.util.EventListener;	//AWT의 event를 담고있는 헤더
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.Button;
-
+import java.util.EventListener;	//AWT의 event를 담고있는 헤더
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
 
 
 public class BottomPanel extends JPanel{
@@ -21,26 +24,61 @@ public class BottomPanel extends JPanel{
 
 	public BottomPanel()	//아래 패널 생성.
 	{
-		this.setBackground(Color.RED);
-		this.setLocation(0,500);	//프레임 내에서 아래쪽 패널위치의 시작점을 설정하는 메소드(X,Y값으로 설정)
-		this.setSize(240,200); //패널 사이즈를 설정!
-		this.setVisible(true);
+		this.setBackground(Color.BLUE);
+		super.setPreferredSize(new Dimension(550,50)); //패널 사이즈를 설정!
 		
-		this.setLayout(new GridLayout(1,4));	//지금 Panel의 배치형태를 GirdLayout(1,4)형태로 설정한다.
-		
-		Panel p1 = new Panel();		//판넬 안에서 새로운 판넬 4개생성
-		p1.setBackground(Color.BLACK);
-		Panel p2 = new Panel();		//판넬의 매개변수는 지정된 레이아웃형태이다.
-		p2.setBackground(Color.DARK_GRAY);
-		Panel p3 = new Panel();
-		p3.setBackground(Color.GREEN);
-		Panel p4 = new Panel();
-		p4.setBackground(Color.PINK);
-		
-		this.add(p1);		//판넬안에 4개의 판넬을 붙여 영역을 설정한다.
-		this.add(p2);		//자연스럽게 가로1칸 세로4칸으로 나뉘어서 추가된다.
-		this.add(p3);
-		this.add(p4);
-	}
+		DrawPanel DP = new DrawPanel();
+		JLabel statusBar = new JLabel( "Mouse outside JPanel" );
+		statusBar.setBackground(Color.GREEN);
+		add( statusBar, BorderLayout.NORTH ); // add label to JFrame
+
 	
-}
+		class MouseHandler implements MouseListener,MouseMotionListener 
+		{
+			public void mouseClicked( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Clicked at [%d, %d]", 
+				event.getX(), event.getY() ) );
+			} // end method mouseClicked
+
+			public void mousePressed( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Pressed at [%d, %d]", 
+						event.getX(), event.getY() ) );
+			} // end method mousePressed
+
+			public void mouseReleased( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Released at [%d, %d]", 
+						event.getX(), event.getY() ) );
+			} // end method mouseReleased
+
+			public void mouseEntered( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Mouse entered at [%d, %d]", 
+				event.getX(), event.getY() ) );
+			} // end method mouseEntered
+			
+			public void mouseExited( MouseEvent event )
+			{
+				statusBar.setText( "Mouse outside JPanel" );
+			} // end method mouseExited
+
+			public void mouseDragged( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Dragged at [%d, %d]", 
+				event.getX(), event.getY() ) );
+			} // end method mouseDragged
+
+			public void mouseMoved( MouseEvent event )
+			{
+				statusBar.setText( String.format( "Moved at [%d, %d]", 
+				event.getX(), event.getY() ) );
+			} // end method mouseMoved
+			
+		}	 
+	    MouseHandler handler = new MouseHandler(); 
+	    DP.addMouseListener( handler );		//import한 DrawPanel의에 이번트핸들러 추가하기. 
+	    DP.addMouseMotionListener( handler ); 
+	}	
+}//bottompanel
