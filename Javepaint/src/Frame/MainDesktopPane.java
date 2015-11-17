041ad98ac957a9eda41ece503f7b47c75895ad5d
KooHanMo <rngksah@naver.com>
2015-11-18 00:24:29 +0900

    insert BackgroundIMG

--- Javepaint/src/Frame/MainDesktopPane.java
+++ Javepaint/src/Frame/MainDesktopPane.java
@@ -1,7 +1,9 @@
 package Frame;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
 
 import SubFrame.DrawInternalFrame;
@@ -13,15 +15,21 @@ public class MainDesktopPane extends JDesktopPane{
 	private DrawInternalFrame drawInternalFrame;
 	private RightUpInternalFrame rightUpInternalFrame;
 	private RightDownInternalFrame rightDownInternalFrame;
+	private ImageIcon backgrountIcon;
+
 	
 	public MainDesktopPane()
 	{
+	
+	
 		this.setVisible(true);
 		this.setBackground(Color.DARK_GRAY);
 		this.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
 		
+		backgrountIcon = new ImageIcon("resource/ui_1_01.png");
 		
 		
+
 		drawInternalFrame = new DrawInternalFrame();
 		rightUpInternalFrame = new RightUpInternalFrame();
 		rightDownInternalFrame = new RightDownInternalFrame();
@@ -30,12 +38,19 @@ public class MainDesktopPane extends JDesktopPane{
 		
 		rightUpInternalFrame.setLocation(1190, 0);
 		rightDownInternalFrame.setLocation(1190,400);
+
 		
-		
+
 		this.add(drawInternalFrame);
 		this.add(rightUpInternalFrame);
 		this.add(rightDownInternalFrame);
 
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		g.drawImage(backgrountIcon.getImage(), 300, 50, null);
 	}
 
 }
--- Javepaint/src/Frame/MainFrame.java
+++ Javepaint/src/Frame/MainFrame.java
@@ -1,9 +1,7 @@
 package Frame;
 
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
-
 import SubFrame.BottomLabel;
 import SubFrame.LeftPanel;
 import SubFrame.TopMenu;
@@ -16,25 +14,25 @@ public class MainFrame extends JFrame{
 	private LeftPanel leftPanel;
 	private BottomLabel bottomLabel;
 	private TopPanel topPanel;
-	private TopMenu menuBar;
-
-
-
+	private TopMenu menuBar;	
 
 	
 	public MainFrame()
 	{
 	
+	
 		//패널 객체 생성
 		leftPanel = new LeftPanel();
 		bottomLabel = new BottomLabel("바닥");
 		topPanel = new TopPanel();
-		
-		
+	
+
 		//패널 위치에 배정
+
 		this.add(leftPanel, BorderLayout.WEST);
 		this.add(bottomLabel, BorderLayout.SOUTH);
 		this.add(topPanel,BorderLayout.NORTH);
+
 		
 		//메뉴 생성
 		menuBar = new TopMenu();
