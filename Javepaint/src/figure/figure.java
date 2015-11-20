43bd8316e5bd5ca3c60d3f4556fdb8f2c09aaf3e
kuhanmo <rngksah@naver.com>
2015-11-20 16:33:25 +0900

    12
    1

--- Javepaint/src/figure/figure.java
+++ Javepaint/src/figure/figure.java
@@ -1,7 +1,19 @@
 package figure;
 
-public interface figure {
-	
-	
+import java.awt.Graphics;
 
+public abstract class  Figure {
+	
+	private int width,height;
+	
+	public Figure(int width, int height)
+	{
+		this.width=width;
+		this.height=height;
+	}
+	
+	
+	public abstract void getSize();
+	public abstract void drawFigure(Graphics g);
+	
 }
